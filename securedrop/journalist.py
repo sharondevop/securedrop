063e06437a00a75e6f98b915035ea886ec8bdfe1
redshiftzero <jen@freedom.press>
2017-11-01 12:28:19 -0700

    Journalist Interface: Use custom validator for OTP secret
    Whitespace should be ignored when computing the 40 characters required for
    the OTP secret.
    
    We can use a custom validator:
    
    https://wtforms.readthedocs.io/en/latest/validators.html#custom-validators
    
    to ensure this field is correct.

--- securedrop/journalist.py
+++ securedrop/journalist.py
@@ -13,7 +13,8 @@ from sqlalchemy.exc import IntegrityError
 from sqlalchemy.sql.expression import false
 
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField, TextField, BooleanField, HiddenField
+from wtforms import (TextAreaField, TextField, BooleanField, HiddenField,
+                     ValidationError)
 from wtforms.validators import InputRequired, Optional, Length
 
 import config
@@ -69,6 +70,12 @@ class ReplyForm(FlaskForm):
     ])
 
 
+def otp_secret_validation(form, field):
+    strip_whitespace = field.data.replace(' ', '')
+    if len(strip_whitespace) != 40:
+        raise ValidationError(gettext('Field must be 40 characters long.'))
+
+
 class NewUserForm(FlaskForm):
     username = TextField('username', validators=[
         InputRequired(message=gettext('This field is required.')),
@@ -81,8 +88,7 @@ class NewUserForm(FlaskForm):
     is_admin = BooleanField('is_admin')
     is_hotp = BooleanField('is_hotp')
     otp_secret = TextField('otp_secret', validators=[
-        Length(min=40, max=40,
-               message=gettext('Field must be 40 characters long.')),
+        otp_secret_validation,
         Optional()
     ])
 
